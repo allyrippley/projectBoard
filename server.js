814867512d21d00c9c9a5ec2cf5a8f30d227d15d
allyrippley <mctransporter@gmail.com>
2015-03-17 17:37:07 -0500

    refactoring

--- server.js
+++ server.js
@@ -13,34 +13,8 @@ require('./server/config/express')(app, config);
 
 require('./server/config/mongoose')(config);
 
-var User = mongoose.model('User');
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    User.findOne({username: username}).exec(function(err, user) {
-      if(user && user.authenticate(password)) {
-        return done(null, user);
-      } else {
-        return done(null, false);
-      }
-    });
-  }
-));
+require('./server/config/passport')();
 
-passport.serializeUser(function(user, done) {
-  if(user) {
-    done(null, user._id)
-  }
-});
-
-passport.deserializeUser(function(id, done) {
-  User.findOne({_id:id}).exec(function(err, user) {
-    if(user) {
-      return done(null, user);
-    } else {
-      return done(null, false);
-    }
-  });
-})
 require('./server/config/routes')(app);
 
 
