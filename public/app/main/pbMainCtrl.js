c673074186856bc48e60d2edabe0b85ff0852d51
allyrippley <mctransporter@gmail.com>
2015-03-22 22:00:30 -0500

    Base MEAN App Complete!

--- public/app/main/pbMainCtrl.js
+++ public/app/main/pbMainCtrl.js
@@ -1,16 +1,4 @@
-angular.module('app').controller('pbMainCtrl', function($scope) {
-  $scope.myVar = "Hello Angular";
-  $scope.projects = [
-    {name: 'a', featured: false, published: new Date('3/14/2015')},
-    {name: 'b', featured: true, published: new Date('3/14/2015')},
-    {name: 'c', featured: false, published: new Date('3/14/2015')},
-    {name: 'd', featured: false, published: new Date('3/14/2015')},
-    {name: 'e', featured: true, published: new Date('3/14/2015')},
-    {name: 'f', featured: false, published: new Date('3/14/2015')},
-    {name: 'g', featured: true, published: new Date('3/14/2015')},
-    {name: 'h', featured: false, published: new Date('3/14/2015')},
-    {name: 'i', featured: false, published: new Date('3/14/2015')},
-    {name: 'j', featured: false, published: new Date('3/14/2015')},
-    {name: 'k', featured: false, published: new Date('3/14/2015')}
-  ]
+angular.module('app').controller('pbMainCtrl', function($scope, pbProject) {
+  $scope.projects = pbProject.query();
+
 });
--- public/app/projects/pbProjectListCtrl.js
+++ public/app/projects/pbProjectListCtrl.js
@@ -1,3 +1,6 @@
 angular.module('app').controller('pbProjectListCtrl', function($scope, pbProject) {
   $scope.projects = pbProject.query();
+
+  $scope.sortOptions= [{value:"title",text: "Sort by Title"}, {value:"published", text: "Sort by Publish Date"}];
+  $scope.sortOrder = $scope.sortOptions[0].value;
 });
